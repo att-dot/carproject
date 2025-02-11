bbdb682183ac02603e20f9fc1401cd110e8feb45
artem <asulakov256@gmail.com>
2025-02-11 17:51:10 +0300

    add search form

--- src/components/CustomSelector/CustomSelector.tsx
+++ src/components/CustomSelector/CustomSelector.tsx
@@ -1,35 +1,57 @@
 import { useId, useState } from "react";
 import "./CustomSelector.scss";
 
-export default function CustomSelector({options, subject, defaultValue}: {options: string[]; subject: string; defaultValue: string}) {
-  // const [isSelecting, setIsSelecting]: [boolean, any] = useState(false);
-  const [selectedValue, setSelectedValue]: [string, any] =
-    useState(defaultValue || "нажмите чтобы выбрать");
-console.log(options)
+export default function CustomSelector({
+  options,
+  subject,
+  defaultValue,
+  setClickedChevron,
+  clickedChevron
+}: {
+  options: string[];
+  subject: string;
+  defaultValue: string;
+  setClickedChevron: (clickedChevron: string)=> void;
+  clickedChevron: string
+}) {
+  const [selectedValue, setSelectedValue]: [string, any] = useState(
+    defaultValue || "нажмите чтобы выбрать"
+  );
+  const [checked, setChecked] = useState(false);
+  const id = "checkboxChevron" + useId();
   const [isClicked, setIsClicked]: [boolean | null, any] = useState(null);
   return (
     <div className="selector ">
-      {selectedValue}
+      <div className="selected-value">{selectedValue}</div>
+
       <Chevrong
-        onClick={() => {
-        }}
+        onClick={() => {}}
         isClicked={isClicked}
         setIsClicked={setIsClicked}
+        checked={checked}
+        setChecked={setChecked}
+        setClickedChevron={setClickedChevron}
+        clickedChevron={clickedChevron}
+        id = {id}
       />
 
-      
       <Options
         onClick={setSelectedValue}
         options={options}
         selectedValue={selectedValue}
-        // optional={() => {
-        //   setIsClicked(false);
-        //   setIsSelecting(false);
-        // }}
+        optional={() => {
+          setIsClicked(false);
+        }}
+        setClickedChevron={setClickedChevron}
         subject={subject}
-        className = {isClicked === null?'': isClicked? 'visibleOptions': 'hiddenOptions'}
+        className={
+          isClicked === null
+            ? ""
+            : clickedChevron === id
+            ? "visibleOptions"
+            : "hiddenOptions"
+        }
       />
-      
     </div>
   );
 }
@@ -38,14 +60,32 @@ type ChevrongType = {
   onClick: () => void;
   isClicked: boolean | null;
   setIsClicked: any;
+  checked: boolean;
+  setChecked: (checked: boolean )=>void
+  id: string;
+  setClickedChevron: (clickedChevron: string) => void;
+  clickedChevron: string
 };
-function Chevrong({ onClick, isClicked, setIsClicked }: ChevrongType) {
-  const id = "checkboxChevron" + useId()
+function Chevrong({ onClick, isClicked, setIsClicked, setClickedChevron, clickedChevron, id  }: ChevrongType) {
+  
   return (
     <div className="chevronContainer">
-      <input id={id} type="checkbox" name="checkboxChevron1" className="checkboxChevron" title="open the menu to choose"/>
+      <input
+        id={id}
+        type="checkbox"
+        name="checkboxChevron1"
+        className="checkboxChevron"
+        title="open the menu to choose"
+        checked={clickedChevron === id}
+        onChange={() => {
+          if(clickedChevron === id){
+            setClickedChevron('')
+          }else{
+            setClickedChevron(id)
+          }
+        }}
+      />
       <label
-       
         htmlFor={id}
         className="chevron"
         onClick={() => {
@@ -62,29 +102,42 @@ type OptionsType = {
   selectedValue: string;
   className: string;
   subject: string;
+  optional: () => void;
+  setClickedChevron: (clickedChevron: string) => void;
 };
-function Options({ onClick, options, selectedValue, className, subject }: OptionsType) {
+function Options({
+  onClick,
+  options,
+  selectedValue,
+  className,
+  subject,
+  optional,
+  setClickedChevron,
+}: OptionsType) {
   const arr = [];
   for (let i = 0; i < options.length; i++) {
     arr.push(
-      <div key={options[i]}>
+      <div key={options[i] + subject}>
         <input
-          id={options[i]}
+          id={options[i] + subject}
           checked={selectedValue === options[i]}
           onChange={() => {
             onClick(options[i]);
+            optional();
+            setClickedChevron('')
           }}
+          
           type="radio"
-          name={'radio' + subject}
+          name={"radio" + subject}
           // onClick={() => {
           //   onClick(options[i]);
           // }}
-          
+
           value={options[i]}
-          key={options[i]}
+          key={options[i] + subject}
         />
         <label
-          htmlFor={options[i]}
+          htmlFor={options[i] + subject}
           className={
             "options-item " + (selectedValue === options[i] ? "selected" : "")
           }
@@ -96,7 +149,9 @@ function Options({ onClick, options, selectedValue, className, subject }: Option
   }
   return (
     <>
-      <div className={"options " + className} key={'options'}>{...arr}</div>
+      <div className={"options " + className} key={"options"}>
+        {...arr}
+      </div>
     </>
   );
 }
--- src/components/SearchSection/SearchSection.tsx
+++ src/components/SearchSection/SearchSection.tsx
@@ -1,83 +1,180 @@
-import {  useState } from "react";
+import { useState } from "react";
 import CustomSelector from "../CustomSelector/CustomSelector";
 import "./SearchSection.scss";
 import "./img/searchnlike.svg";
+import MainButton from "../MainButton/MainButton";
 
 export default function SearchSection({}) {
+  //   const b = {
+  //     "carAge": "",
+  //     "checkboxChevron1": "on",
+  //     "radiocar": "BMV",
+  //     "radionumber": "Vesta",
+  //     "radioyear": "2017",
+  //     "radiobodywork": "Кузов",
+  //     "radiobox": "Коробка",
+  //     "radioengine": "Двигатель",
+  //     "radiodrive": "Привод",
+  //     "radiovolume": "Объем"
+  // }
   const b = {
-    "carAge": "Новые",
-    "checkboxChevron1": "on",
-    "radiocar": "BMV",
-    "radionumber": "Vesta",
-    "radioyear": "2017"
-}
-  const list = ["Все", "Новые", "С пробегом"]
-  // const [selectedValue, setSelectedValue] = useState(b.carAge)
-  
+    radiocar: "BMV",
+    radionumber: "Vesta",
+    radioyear: "2017",
+    radiobodywork: "средний",
+    radiobox: "1",
+    radioengine: "3",
+    radiodrive: "2",
+    radiovolume: "4",
+  };
+  const list = ["Все", "Новые", "С пробегом"];
+
+  const [clickedChevron, setClickedChevron] = useState("");
   return (
     <form className="searchBar">
       <div className="container">
-        {/* <div className="item"> */}
-          {/* <input id="radio1" type="radio" name="carAge" value={"Все"} />
-          <label htmlFor="radio1">Все</label> */}
-          {/* <RadioButton name={'carAge'} labelV={"Все"}/>
-        </div>
-        <div className="item"> */}
-          {/* <input id="radio2" type="radio" name="carAge" value={"Новые"} />
-          <label htmlFor="radio2">Новые</label> */}
-          {/* <RadioButton name={'carAge'} labelV={"Новые"}/>
-        </div>
-        <div className="item"> */}
-          {/* <input id="radio3" type="radio" name="carAge" value={"С пробегом"} />
-          <label htmlFor="radio3">С пробегом</label> */}
-          {/* <RadioButton name={'carAge'} labelV={"С пробегом"}/>
-        </div> */}
         <FirstSelection list={list} b={b} />
-         {/* {list.map((el, i) =>{
-          return <div className="item" key={i}>
-          
-          <RadioButton name={'carAge'} labelV={el} setSelectedValue={setSelectedValue} selectedValue={selectedValue}/>
-        </div>
-         })} */}
       </div>
-      <button className="searchnlike" type="button">
+      <button className="searchnlike" type="button" onClick={searchnlike}>
         <div className="like"></div>
         <div className="savesearch">Сохранить поиск</div>
       </button>
       <div className="firstSelectingRow">
-      <CustomSelector options={["LADA (ВАЗ)", "BMV", "Mercedes", "Priora"]} subject={'car'} defaultValue={b.radiocar}/>
-      <CustomSelector options={['Vesta', '2', '3', '4']} subject={'number'} defaultValue={b.radionumber}/>
-      <CustomSelector options={['2015', '2016', '2017', '2018']} subject={'year'} defaultValue={b.radioyear}/>
+        <CustomSelector
+          options={["LADA (ВАЗ)", "BMV", "Mercedes", "Priora"]}
+          subject={"car"}
+          defaultValue={b.radiocar}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+        <CustomSelector
+          options={["Vesta", "2", "3", "4"]}
+          subject={"number"}
+          defaultValue={b.radionumber}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+        <CustomSelector
+          options={["2015", "2016", "2017", "2018"]}
+          subject={"year"}
+          defaultValue={b.radioyear}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
       </div>
-      
+      <div className="secondSelectingRow">
+        <CustomSelector
+          options={["нет", "маленький", "средний", "большой"]}
+          subject={"bodywork"}
+          defaultValue={b.radiobodywork}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+        <CustomSelector
+          options={["1", "2", "3", "4"]}
+          subject={"box"}
+          defaultValue={b.radiobox}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+        <CustomSelector
+          options={["1", "2", "3", "4"]}
+          subject={"engine"}
+          defaultValue={b.radioengine}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+        <CustomSelector
+          options={["1", "2", "3", "4"]}
+          subject={"drive"}
+          defaultValue={b.radiodrive}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+        <CustomSelector
+          options={["1", "2", "3", "4"]}
+          subject={"volume"}
+          defaultValue={b.radiovolume}
+          setClickedChevron={setClickedChevron}
+          clickedChevron={clickedChevron}
+        />
+      </div>
+      <div className="thirdSelectingRow">
+        <div>
+          <label htmlFor="pricefrom">Цена от, ₽</label>
+        <input placeholder="Цена от, ₽" className="pricefrom" id="pricefrom"/>
+        <input placeholder="Цена до, ₽" className="priceto"/>
+        </div>
+        <div>
+        <input placeholder="Пробег от, ₽" className="milagefrom"/>
+        <input placeholder="Пробег до, ₽" className="milageto"/>
+        </div>
+        {/* <input placeholder="Цена от, ₽" className="pricefrom"/>
+        <input placeholder="Цена до, ₽" className="priceto"/> */}
+         <MainButton onClick={() => console.log("You clicked on the pink circle!")} label='Показать предложения' arrow={false} btnStyle='showsugg' link='any' />
+      </div>
     </form>
   );
 }
 type RadioButtonType = {
-name: string;
-labelV: string;
-selectedValue: string;
-setSelectedValue: (labelV: string) => void
-}
-function RadioButton({name, labelV, selectedValue, setSelectedValue }: RadioButtonType) {
+  name: string;
+  labelV: string;
+  selectedValue: string;
+  setSelectedValue: (labelV: string) => void;
+};
+function RadioButton({
+  name,
+  labelV,
+  selectedValue,
+  setSelectedValue,
+}: RadioButtonType) {
   return (
     <>
-      <input id={name + labelV} type="radio" name={name} value={labelV} key={name + labelV} checked={selectedValue === labelV} onChange={()=>{setSelectedValue(labelV)}}/>
+      <input
+        id={name + labelV}
+        type="radio"
+        name={name}
+        value={labelV}
+        key={name + labelV}
+        checked={selectedValue === labelV}
+        onChange={() => {
+          setSelectedValue(labelV);
+        }}
+      />
       <label htmlFor={name + labelV}>{labelV}</label>
     </>
   );
 }
 
-function FirstSelection({list, b}: {list: string[]; b: any}){
-  const [selectedValue, setSelectedValue] = useState(b.carAge)
+function FirstSelection({ list, b }: { list: string[]; b: any }) {
+  const [selectedValue, setSelectedValue] = useState(b.carAge);
   return (
     <>
-    {list.map((el, i) =>{
-          return <div className="item" key={i}>
-          
-          <RadioButton name={'carAge'} labelV={el} setSelectedValue={setSelectedValue} selectedValue={selectedValue}/>
-        </div>
-         })}
+      {list.map((el, i) => {
+        return (
+          <div className="item" key={i}>
+            <RadioButton
+              name={"carAge"}
+              labelV={el}
+              setSelectedValue={setSelectedValue}
+              selectedValue={selectedValue}
+            />
+          </div>
+        );
+      })}
     </>
-  )
+  );
+}
+
+function searchnlike() {
+  const form: HTMLFormElement | null = document.querySelector("form.searchBar");
+  if (form !== null) {
+    const a = new FormData(form, null);
+    const b = {};
+    for (let [key, v] of a) {
+      Object.assign(b, { [key]: v });
+      console.log(key, v);
+    }
+    console.log(b);
+  }
 }
